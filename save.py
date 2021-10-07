4ad6a72f657fa854c8d5e610defeaaab61672ce8
Silviusr <91711986+Silviusr@users.noreply.github.com>
2021-10-07 10:27:55 +0600

    Add files via upload

---
+++ save.py
@@ -0,0 +1,19 @@
+# # c = int(input('enter count:\n'))
+# # name_list = []
+# # for i in range(c):
+# #     name_list.append(str(input("enter name: ")))
+# # for j in name_list:
+# #     print(j)
+
+
+
+# add = str(input('do you want add name?(y) or (n)\n'))
+# all_list = []
+# while add == "y":
+#     all_list.append(str(input('enter your name: ')))
+#     all_list.append(str(input('enter your last name: ')))
+#     all_list.append(str(input('enter your age: ')))
+#     ex = str(input('do you want continue?(y) or (n): '))
+#     if ex == 'n':
+#         break
+# print(all_list)
